9ca81cd8ecf13a897685a2c2122ddc421e7c98f8
Tinam Durge <97393169+tinamdurge@users.noreply.github.com>
2023-11-25 14:25:30 +0530

    Added: Create contacts in address book

---
+++ Contacts.java
@@ -0,0 +1,60 @@
+public class Contacts {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private long phoneNumber;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacts { " +
+                "firstName ='" + firstName + '\'' +
+                ", lastName ='" + lastName + '\'' +
+                ", address ='" + address + '\'' +
+                ", city ='" + city + '\'' +
+                ", state ='" + state + '\'' +
+                ", zip =" + zip +
+                ", phoneNumber =" + phoneNumber +
+                ", email ='" + email + '\'' +
+                '}';
+    }
+}
